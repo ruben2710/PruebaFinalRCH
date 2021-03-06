5222f496e18d6584da5058ca57809a494fd04093
ruben2710 <77392528+ruben2710@users.noreply.github.com>
2021-03-07 00:16:10 +0100

    PruebaFinal_RCH

--- PlantillaRCH/PlantillaRCH.js
+++ PlantillaRCH/PlantillaRCH.js
@@ -1,7 +1,5 @@
 var mapMain;
 var tb;
-var lyrCities;
-var ftStates;
 require(["esri/map",
     //para introducir el servicio "http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer"
     "esri/layers/ArcGISDynamicMapServiceLayer",
@@ -22,6 +20,8 @@ require(["esri/map",
     "esri/tasks/query",
     //Para introducir el widget Overview
     "esri/dijit/OverviewMap",
+    //Para introducir el widget HomeButton
+    "esri/dijit/HomeButton",
     //Para introducir el widget Search
     "esri/dijit/Search",
     "esri/dijit/PopupTemplate",
@@ -47,116 +47,163 @@ require(["esri/map",
     "dijit/layout/BorderContainer",
     "dojo/domReady!"],
     function (Map, ArcGISDynamicMapServiceLayer, Extent, Scalebar, Legend, BasemapGallery, FeatureLayer, FeatureTable, Draw,
-        Graphic, graphicsUtils, query, OverviewMap, Search, PopupTemplate, on, ready, parser, dom, Memory, locale, Grid, Selection,
+        Graphic, graphicsUtils, query, OverviewMap, HomeButton, Search, PopupTemplate, on, ready, parser, dom, Memory, locale, Grid, Selection,
         Color, declare, array, SimpleFillSymbol, SimpleLineSymbol, SimpleMarkerSymbol
     ) {
 
-         /*
-        * Paso 10: En el panel tareas tenemos un botón que cuando pulsemos sobre él, activaremos la herramienta Draw para poder 
-        seleccionar desde el mapa ciudades (con temática de puntos amarillos y con el identificador 0 del MapServer) creando una extensión. 
-        Una vez creada la extensión resaltaremos en el mapa las ciudades que están bajo la extensión dibujada.
-        */
-
-       on(dojo.byId("pintaYQuery"), "click", fPintaYQuery);
-       on(dojo.byId("progButtonNode"), "click", fQueryEstados);
-
-       function fPintaYQuery() {
-        alert("Evento del botón Seleccionar ciudades");
-      }
-      function fQueryEstados() {
-        alert("Evento del botón Ir a estado");
-      }
-
-       function fPintaYQuery(){
-    
-        tb = new Draw (mapMain);
-        tb.on("draw-end",function (evt) {
-          
-          // gemoetria del evento
-          var geometryInputCities = evt.geometry;
-          
-          //definicion de la simbologia del poligono
-          var tbSymbolCities = new SimpleFillSymbol(
-            SimpleFillSymbol.STYLE_SOLID, 
-            new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT, new Color([255, 255, 0]), 2), new Color([255, 255, 0, 0.2])
-          );
-              
-          //borra los poligonos dibujados
-          mapMain.graphics.clear();
-          
-          //Para añadir el Graphic layer al mapa. 
-          var graphicPolygon = new Graphic(geometryInputCities, tbSymbolCities);
-          mapMain.graphics.add(graphicPolygon);
-          
-          //funcion para selecionar las ciudades
-          //Simbolo de seleción  de las ciudades
-          var sbSelectCities = new SimpleMarkerSymbol({
-            "type": "esriSMS",
-            "style": "esriSMSCircle",
-            "color": [252,146,114],
-            "size": 10,
-            "outline": {
-              "color": [222,45,38],
-              "width": 1
-            }
-          });
-              
-          lyrCities.setSelectionSymbol(sbSelectCities);
-          
-          //Hace la seleción apartir de la area dibuja en el mapa
-          var queryCities = new Query();
-          queryCities.geometry = geometryInputCities;
-          // ftCities.on("selection-complete");
-          lyrCities.selectFeatures(queryCities, FeatureLayer.SELECTION_NEW, function(features){
-            TableCities.filterSelectedRecords()
-          });
-        });
-        tb.activate(Draw.POLYGON); 
-      }
-          
-      // Limpiar selección de las ciudades
-      on(dojo.byId("clear"),"click",fClearCities);
-      
-      function fClearCities (){
-        lyrCities.clearSelection();
-        mapMain.graphics.clear();
-        TableCities.clearFilter()
-        tb.deactivate()
-      }
-
-       /*
-       * Paso 11: En el panel inferior de la aplicación podremos escribir el nombre de un estado y 
-       al pulsar sobre el botón “Ir a estado”, seleccionará el estado introducido en la caja de texto, (ejemplo: “Washington”). 
-       Al mostrar el estado seleccionado en el mapa centrar la extensión sobre ese estado. Esta capa tiene el identificador 2 del MapServer.
+        /*
+       * Paso 11: En el panel tareas tenemos un botón que cuando pulsemos sobre él, activaremos la herramienta Draw para poder 
+       seleccionar desde el mapa ciudades (con temática de puntos amarillos y con el identificador 0 del MapServer) creando una extensión. 
+       Una vez creada la extensión resaltaremos en el mapa las ciudades que están bajo la extensión dibujada.
        */
 
+      on(dojo.byId("pintaYQuery"),"click",fPintaYQuery);
+      
+      var tb = new Draw(mapMain);
+      
+      function fPintaYQuery() {
 
-
-       function fQueryEstados() {
-
-           
-           var inputState = dojo.byId("dtb").value;
-
-           var sbState = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
-               new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT,
-                   new Color([255, 0, 0]), 2), new Color([255, 255, 0, 0.5])
-           );
-
+      mapMain.setInfoWindowOnClick(false);
         
-           ftStates.setSelectionSymbol(sbState);
 
-           
-           var queryState = new Query();
-           queryState.where = "state_name =" + "'" + inputState + "'"; 
+          tb.on("draw-end", displayPolygon);
+          tb.activate(Draw.POLYGON);
+      };
 
-           ftStates.selectFeatures(queryState, FeatureLayer.SELECTION_NEW, function (selection) {
-               var centerSt = graphicsUtils.graphicsExtent(selection).getCenter();
-               var extentSt = esri.graphicsExtent(selection);
+      function displayPolygon(evt) {
+        var geometryInput = evt.geometry;
 
-               mapMain.setExtent(extentSt.getExtent().expand(2));
-               mapMain.centerAt(centerSt);
-           });
-       };
+      
+
+              var tbDrawSymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, 
+                  new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT, new Color([255, 255, 0]), 2), new Color([255, 255, 0, 0.2]));
+
+              
+
+              mapMain.graphics.clear();
+
+            
+
+              var graphicPolygon = new Graphic(geometryInput, tbDrawSymbol);
+              mapMain.graphics.add(graphicPolygon);
+
+              selectCities(geometryInput);
+              };
+
+              function selectCities(geometryInput) {
+
+                
+                var symbolSelected = new SimpleMarkerSymbol({
+                    "type": "esriSMS",
+                    "style": "esriSMSCircle",
+                    "color": [255, 115, 0, 128],
+                    "size": 6,
+                    "outline": {
+                        "color": [255, 0, 0, 214],
+                        "width": 1
+                    }
+                });
+
+                
+                ftCities.setSelectionSymbol(symbolSelected);
+
+                var queryCities = new Query();
+                queryCities.geometry = geometryInput;  
+
+
+                ftCities.on("selection-complete", populateGrid)
+
+
+                lyrCities.selectFeatures(queryCities, FeatureLayer.SELECTION_NEW)
+
+              }
+
+              function populateGrid(results) {
+
+               
+                
+
+                var dataCities = array.map(results.features, function (feature) {
+                    return {
+
+                        
+
+                        "areaname": feature.attributes[outFieldsCities[0]],
+                        "class": feature.attributes[outFieldsCities[1]],
+                        "st": feature.attributes[outFieldsCities[2]],
+                        "capital": feature.attributes[outFieldsCities[3]],
+                        
+                        
+                        
+
+
+
+                    }
+                });
+
+                 // Pass the data to the grid
+                 var memStore = new Memory({
+                  data: dataCities
+                });
+                gridCities.set("store", memStore);
+                
+
+    
+
+
+
+                
+            };
+  
+  
+      on(dojo.byId("clear"),"click",fClearCities); 
+
+      
+      function fClearCities(){      
+        ftCities.clearSelection();
+        mapMain.graphics.clear();
+        TableCities.clearFilter();
+        tb.deactivate();
+        
+
+        mapMain.setInfoWindowOnClick(true);
+        
+      };
+      
+
+        /*
+        * Paso 12: En el panel inferior de la aplicación podremos escribir el nombre de un estado y 
+        al pulsar sobre el botón “Ir a estado”, seleccionará el estado introducido en la caja de texto, (ejemplo: “Arizona”). 
+        Al mostrar el estado seleccionado en el mapa centrar la extensión sobre ese estado. Esta capa tiene el identificador 2 del MapServer.
+        */
+
+       on(dojo.byId("progButtonNode"), "click", fQueryEstados);
+
+        function fQueryEstados() {
+
+
+            var inputState = dojo.byId("dtb").value;
+
+            var sbState = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+                new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT,
+                    new Color([255, 0, 0]), 2), new Color([255, 255, 0, 0.5])
+            );
+
+
+            ftStates.setSelectionSymbol(sbState);
+
+
+            var queryState = new Query();
+            queryState.where = "state_name =" + "'" + inputState + "'";
+
+            ftStates.selectFeatures(queryState, FeatureLayer.SELECTION_NEW, function (selection) {
+                var centerSt = graphicsUtils.graphicsExtent(selection).getCenter();
+                var extentSt = esri.graphicsExtent(selection);
+
+                mapMain.setExtent(extentSt.getExtent().expand(2));
+                mapMain.centerAt(centerSt);
+            });
+        };
 
 
         //Paso 2: Especificar la extensión inicial.
@@ -187,17 +234,19 @@ require(["esri/map",
         //* Estados:	"http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/2"
         //* Paises:		"http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/3"
          */
-
+        var lyrUSA = new ArcGISDynamicMapServiceLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer", {
+        opacity: 0.25});
+        lyrUSA.setVisibleLayers([1,3]);
 
         /*
           * Paso 4: Añadir la capa de ciudades el mapa.
           */
-        var lyrCities = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/0", {
+         var ftCities = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/0", {
             outFields: ["*"]
         });
 
         var ftStates = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/2", {
-            outFields: ["*"] 
+            outFields: ["*"]
         });
 
 
@@ -206,13 +255,14 @@ require(["esri/map",
             mapMain.resize();
             mapMain.reposition();
 
+
             /*
             * Paso 3: Uso del método addLayer para mostrar las variables en el mapa.
             
-            mapMain.addLayer(lyrCities);
+            mapMain.addLayer(ftCities);
             mapMain.addLayer(ftStates);
             */
-            mapMain.addLayers([lyrCities, ftStates]);
+            mapMain.addLayers([lyrUSA, ftCities, ftStates]);
 
         });
         /*
@@ -258,7 +308,7 @@ require(["esri/map",
         Overview.startup();
 
         /*
-        * Paso 9: Añadir Widget. Buesqueda en el mapa. Class: Search dentro del Dijit.
+        * Paso 9: Añadir Widget. Busqueda en el mapa. Class: Search dentro del Dijit.
         Este widget nos proporciona rapidez de la funcionalidad de la geodificación al añadirla a un mapa.
         Es un widget de búsqueda que utiliza geolocalizador de argisonline. 
         */
@@ -268,40 +318,56 @@ require(["esri/map",
         }, "search");
 
         search.startup();
+
         /*
-       * Paso 12: En la pestaña de la izquierda que define la funcion de selección de la capa de ciudades,
+        * Paso 10: Añadir Widget. Retorno a la extensión inicial. Class: HomeButton dentro del Dijit.
+         Este widget nos proporciona un botón simple para volver a la extensión inicial predeterminada del mapa.  
+        */
+        var homeButton = new HomeButton({
+            map: mapMain,
+            extent: extensioninicial,
+            visible: true
+        }, "HomeButton");
+        homeButton.startup();
+        
+        
+
+        /*
+       * Paso 13: En la pestaña de la izquierda que define la funcion de selección de la capa de ciudades,
         insertamos una tabla que nos defina los campos de las entidades seleccionadas.
        */
-      lyrCities.on("load", function(){
-        TableCities =  new FeatureTable({
-          featureLayer : lyrCities,
-          map : mapMain,
-          outFields:["st", "areaname","class", "pop2000"],
-          syncSelection: true,
-          zoomToSelection:true,
-          fieldInfos: [
-            {
-              name: 'st',
-              alias: 'Estado'
-            },
-            {
-              name: 'areaname',
-              alias: 'Ciudad'
-            },{
-              name: 'class',
-              alias: 'Clase'
-            },{
-              name: 'pop2000',
-              alias: 'Habitantes'
-            }],
-          }, "TableNode");
-          
-          TableCities.startup();
-        }) 
+        ftCities.on("load", function () {
+            var TableCities = new FeatureTable({
+                featureLayer: ftCities,
+                map: mapMain,
+                outFields: ["st", "areaname", "class", "pop2000"],
+                syncSelection: true,
+                zoomToSelection: true,
+                fieldInfos: [
+                    {
+                        name: 'st',
+                        alias: 'Estado'
+                    },
+                    {
+                        name: 'areaname',
+                        alias: 'Ciudad'
+                    }, {
+                        name: 'class',
+                        alias: 'Clase'
+                    }, {
+                        name: 'pop2000',
+                        alias: 'Habitantes'
+                    }],
+            }, "TableNode");
+
+            TableCities.startup();
+        })
+
         
     });
 
 
+ 
 
 
 
